f5653d0143f3db810cb8e9d57b45040e5d35dd64
Matheus Henrique Schumliak <matheeus.hs@gmail.com>
2025-11-22 18:05:02 -0300

    Adiciona projeto HoraCerta.CrossCutting à solução
    - Adicionado o projeto `HoraCerta.CrossCutting` para centralizar
      e gerenciar dependências compartilhadas.
    - Criado o arquivo `HoraCerta.CrossCutting.csproj` com suporte
      ao .NET 8.0 e referências a outros projetos da solução.
    - Alterado `Program.cs` para incluir injeção de dependência
      personalizada via `AddDependencyInjection`.
    - Criado `ServiceCollectionExtensions.cs` para registrar
      dependências de serviços e repositórios.
    - Organizado o projeto em uma nova pasta `05 -CrossCutting`.
    - Adicionada a seção `ExtensibilityGlobals` no arquivo da solução.
    - Melhorias gerais na estrutura do código para modularidade
      e legibilidade.

---
+++ HoraCerta.CrossCutting/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HoraCerta.CrossCutting.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+	{
+		//Registro de dependências:
+		#region Application Services
+		//services.AddScoped<IProfissionalService, ProfissionalService>();
+		#endregion
+
+		#region Data Repositories
+		//services.AddScoped<IProfissionalRepository, ProfissionalRepository>();
+		#endregion
+
+		// Adicione outros serviços, handlers, clients, etc.
+
+		return services;
+	}
+}
